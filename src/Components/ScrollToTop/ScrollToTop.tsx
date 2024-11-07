56ee8fac1d4a26768d906ab29810f61fa5ac8af9
juanuranidev <juanuranidev@gmail.com>
2024-11-06 23:54:38 -0300

    Deleted old frontend files

--- src/Components/ScrollToTop/ScrollToTop.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-
-export default function ScrollToTop() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [window.location.href]);
-
-  return null;
-}
