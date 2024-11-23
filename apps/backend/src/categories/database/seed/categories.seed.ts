2d104f34cad52c826b9344df185cc032cd49a17b
juanuranidev <juanuranidev@gmail.com>
2024-11-22 21:19:25 -0300

    Added seeds

---
+++ apps/backend/src/categories/database/seed/categories.seed.ts
@@ -0,0 +1,41 @@
+export const CATEGORIES_SEED = [
+    {
+        name: "PRIMAVERA VERANO '25",
+    },
+    {
+        name: "Remeras"
+    },
+    {
+        name: "Hoodies y Buzos"
+    },
+    {
+        name: "Abrigos"
+    },
+    {
+        name: "Denim"
+    },
+    {
+        name: "Pantalones"
+    },
+    {
+        name: "Deportivo"
+    },
+    {
+        name: "Shorts"
+    },
+    {
+        name: "Camisas"
+    },
+    {
+        name: "Calzado"
+    },
+    {
+        name: "Accesorios"
+    },
+    {
+        name: "Nuevos Ingresos"
+    },
+    {
+        name: "Otoño Invierno '24"
+    }
+];
