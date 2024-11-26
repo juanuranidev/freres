2339f54a5da8ce7a52978de9b903f52819b2b8e6
juanuranidev <juanuranidev@gmail.com>
2024-11-25 23:53:37 -0300

    Working on seed module

--- apps/backend/src/config/database/seed/seed.module.ts
+++ apps/backend/src/config/database/seed/seed.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { SeedService } from './services/seed.service';
-import { SeedController } from './seed.controller';
+import { CategoriesModule } from '../../../categories/categories.module';
+import { SeedController } from './controllers/seed.controller';
+import { SeedRepository } from './repository/seed.repository';
+import { SizesModule } from 'sizes/sizes.module';
+import { ProductsModule } from 'products/products.module';
 
 @Module({
+  imports: [CategoriesModule, SizesModule, ProductsModule],
   controllers: [SeedController],
-  providers: [SeedService],
+  providers: [SeedService, SeedRepository],
+  exports: [SeedService]
 })
 export class SeedModule { }
