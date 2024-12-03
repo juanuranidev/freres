373a01c505be584a01f1b5fa4bfefaa05cea6111
juanuranidev <juanuranidev@gmail.com>
2024-12-02 23:50:04 -0300

    Updated app.module and main.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ProductsModule } from './products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { OrdersModule } from './orders/orders.module';
+import { CategoriesModule } from './categories/categories.module';
+import { SizesModule } from './sizes/sizes.module';
+import { OutfitsModule } from './outfits/outfits.module';
+import { StatesModule } from './states/states.module';
+import { SeedModule } from './config/database/seed/seed.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: process.env.SYNC_MIGRATIONS === 'true',
+      logging: true,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+    }),
+    SeedModule,
+    CategoriesModule,
+    SizesModule,
+    ProductsModule,
+    OutfitsModule,
+    OrdersModule,
+    StatesModule,
+  ],
 })
 export class AppModule { }
