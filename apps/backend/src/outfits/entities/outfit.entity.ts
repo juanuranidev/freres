347c279fcc9459980c979ae60a1f7124bcb8f5b3
juanuranidev <juanuranidev@gmail.com>
2024-12-08 23:51:31 -0300

    Updated outfit entity

--- apps/backend/src/outfits/entities/outfit.entity.ts
+++ apps/backend/src/outfits/entities/outfit.entity.ts
@@ -1,14 +1,21 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
 import { OutfitProduct } from './outfit-product.entity';
+import { ApiProperty } from '@nestjs/swagger';
+import { Product } from 'products/entities/product.entity';
 
 @Entity({ name: 'outfits' })
 export class Outfit {
+    @ApiProperty()
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ApiProperty()
     @Column('varchar', { length: 255, unique: true })
     image_url: string;
 
     @OneToMany(() => OutfitProduct, (outfitProducts) => outfitProducts.outfit)
-    outfitProducts: OutfitProduct[];
+    outfits: OutfitProduct[];
+
+    @ApiProperty({ type: () => [Product] })
+    products: Product[];
 }
