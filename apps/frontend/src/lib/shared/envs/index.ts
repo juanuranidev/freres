7a5a98a1ba241dec42f8eea5ca7195188190ff9b
juanuranidev <juanuranidev@gmail.com>
2024-12-11 23:50:46 -0300

    Updated fonts, added envs and toast

---
+++ apps/frontend/src/lib/shared/envs/index.ts
@@ -0,0 +1,5 @@
+const ENVS = {
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_URL,
+}
+
+export default ENVS;
---
+++ apps/frontend/src/lib/shared/toast/index.ts
@@ -0,0 +1,9 @@
+import { toast } from "sonner";
+
+export const createSuccessToastLib = (message: string): string | number => {
+    return toast.success(message);
+};
+
+export const createErrorToastLib = (message: string): string | number => {
+    return toast.error(message);
+};
