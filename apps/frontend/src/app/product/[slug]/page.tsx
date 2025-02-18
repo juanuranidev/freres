e3f8a330b61ca9dd86da0fbf3cce84a02e82abc8
juanuranidev <juanuranidev@gmail.com>
2025-02-17 23:38:10 -0300

    Added accordion ui component and finished product details page

--- apps/frontend/src/app/product/[slug]/page.tsx
+++ apps/frontend/src/app/product/[slug]/page.tsx
@@ -1,21 +1,21 @@
-import Content from './components/content/content';
-import Skeleton from './components/skeleton/skeleton';
+import Content from '@/app/product/[slug]/components/content/content';
+import Skeleton from '@/app/product/[slug]/components/skeleton/skeleton';
 import { Suspense } from 'react';
 
 interface ProductPageProps {
   params: {
-    id: string;
+    slug: string;
   };
 }
 
 export default async function Product({ params }: ProductPageProps) {
-  const { id } = params;
+  const { slug } = params;
 
   return (
     <main className="relative w-full">
       <div className="max-w-[90rem] mx-auto px-2 py-10 min-h-screen">
         <Suspense fallback={<Skeleton />}>
-          <Content id={id} />
+          <Content slug={slug} />
         </Suspense>
       </div>
     </main>
