b65763a2a16d3d54ebaf6aecb396b34a76c034d4
juanuranidev <juanuranidev@gmail.com>
2025-02-18 23:59:08 -0300

    Working on navbar component

--- apps/frontend/src/app/components/navbar/components/buttons/components/cart-icon/cart-icon.tsx
+++ apps/frontend/src/app/components/navbar/components/buttons/components/cart-icon/cart-icon.tsx
@@ -1,15 +1,29 @@
+'use client';
+
+import { useProductCartContext } from '@/contexts/product/product-cart.context';
 import { Icons } from '@/lib/shared/icons';
-import React from 'react';
+import React, { useState } from 'react';
+import CartDrawer from './cart-drawer/cart-drawer';
 
 export default function CartIcon() {
+  const { cart } = useProductCartContext();
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <button className="group p-2 relative rounded-full active:scale-95 transition-transform duration-150">
-      <span className="sr-only">Cart</span>
-      <div className="absolute inset-0 rounded-full transition-colors group-hover:bg-gray-100" />
-      <Icons.cart className="h-5 w-5 stroke-[1.5] transition-colors relative group-hover:text-gray-600" />
-      <span className="absolute -top-1 -right-1 bg-black text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
-        1
-      </span>
-    </button>
+    <>
+      <button
+        className="group p-2 relative rounded-full active:scale-95 transition-transform duration-150"
+        onClick={() => setOpen(true)}
+      >
+        <span className="sr-only">Cart</span>
+        <div className="absolute inset-0 rounded-full transition-colors group-hover:bg-gray-100" />
+        <Icons.cart className="h-5 w-5 stroke-[1.5] transition-colors relative group-hover:text-gray-600" />
+        <span className="absolute -top-1 -right-1 bg-black text-white text-xs rounded-full h-4 w-4 flex items-center justify-center">
+          {cart.length}
+        </span>
+      </button>
+
+      <CartDrawer open={open} onClose={() => setOpen(false)} />
+    </>
   );
 }
