5cecde2b1112640fbd6970d8c1676883157181a1
juanuranidev <juanuranidev@gmail.com>
2025-02-22 23:09:48 -0300

    Updated page and shop view

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,18 +1,47 @@
+import ProductsList from '@/components/shared/products-list/products-list';
 import Outfits from './components/outfits/outfits';
 import Stores from './components/stores/stores';
+import Image from 'next/image';
+import {
+  ReadAllProductsResponse,
+  readAllProductsService
+} from '@/services/product/product.services';
+import Link from 'next/link';
 
 export default async function Home() {
+  const products: ReadAllProductsResponse = await readAllProductsService({
+    limit: 4
+  });
+
   return (
     <main className="relative w-full">
-      <div className="relative w-full h-[100dvh]">
-        <video
-          className="w-full h-full object-cover"
-          src="https://freres.ar/wp-content/uploads/2024/11/BANNERWEBVIDEO21-1.webm"
-          autoPlay
-          loop
-          muted
-          playsInline
+      <div className="relative w-full h-[90dvh]">
+        <Image
+          fill
+          alt="Banner"
+          className="object-cover max-h-[90dvh]"
+          src="https://res.cloudinary.com/dhodvztdx/image/upload/v1740105898/freres_new/N6A1282_1_1_hadhse.jpg"
         />
+        <div className="absolute inset-0 bg-black/10" />
+        <div className="absolute inset-0 flex flex-col items-center justify-center gap-8">
+          <h1 className="text-6xl font-medium text-white">Frères Beach Club</h1>
+          <Link
+            href="/shop"
+            className="px-8 py-3 text-white border border-white hover:bg-white hover:text-black transition-colors duration-300 text-lg"
+          >
+            Ver productos
+          </Link>
+        </div>
+      </div>
+      <div>
+        <div className="flex justify-start pt-20 pl-10 pb-10">
+          <p className="text-2xl font-medium">Novedades</p>
+          <p className="text-2xl font-light mx-2">|</p>
+          <p className="text-2xl font-light">Productos</p>
+        </div>
+        <div className="max-w-[90rem] mx-auto px-2">
+          <ProductsList products={products.data} isLoading={false} />
+        </div>
       </div>
       <Outfits />
       <Stores />
--- apps/frontend/src/app/shop/page.tsx
+++ apps/frontend/src/app/shop/page.tsx
@@ -3,14 +3,33 @@
 import ProductsList from '@/components/shared/products-list/products-list';
 import { useReadProducts } from '@/hooks/products/use-read-products';
 
-export default function Shop() {
-  const { products, isLoading, handleIncrementLimit, hasMore } =
-    useReadProducts({});
+interface ShopProps {
+  searchParams: {
+    category?: string;
+    title?: string;
+  };
+}
+
+export default function Shop({ searchParams }: ShopProps) {
+  const { category, title } = searchParams;
+
+  const handleFormatCategory = (category: string) => {
+    return (
+      category &&
+      category.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase())
+    );
+  };
+
+  const { products, isLoading, handleIncrementLimit, hasMoreProducts } =
+    useReadProducts({
+      category: handleFormatCategory(category!),
+      title
+    });
 
   return (
     <div className="max-w-[90rem] mx-auto px-2 py-10 min-h-screen">
       <ProductsList products={products} isLoading={isLoading} />
-      {!isLoading && products.length > 0 && hasMore && (
+      {!isLoading && products.length > 0 && hasMoreProducts && (
         <div className="flex justify-center mt-8">
           <button
             onClick={handleIncrementLimit}
